d9be2b299086773c8c8b8f40994d84565f4d80da
Vikash-Android7500 <Vikash75.jm@gmail.com>
2024-11-08 23:32:15 +0530

    basic operator

---
+++ 01PROGRAM/Operators/basic.js
@@ -0,0 +1,9 @@
+const number1 = 10;
+const number2 = 5;
+
+console.log(`Addition: ${number1 + number2}
+     Subtraction: ${number1 - number2}
+     Multiplication: ${number1 * number2}
+     Divition: ${number1 / number2}
+     Modulo: ${number1 % number2}
+     `)
